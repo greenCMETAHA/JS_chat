4f9980edda7d6d2ae8a22d60b72d5737e9888e1f
Glebas <Glebas@tut.by>
2016-01-24 20:51:11 +0200

    base for SPA

--- app/js/spa.js
+++ app/js/spa.js
@@ -11,11 +11,12 @@
 /*global $, spa */
 var spa = (function () {
     var initModule = function ( $container ) {
-        $container.html(
-            '<h1 style="display:inline-block; margin:25px;">'
-            + 'hello world!'
-            + '</h1>'
-        );
+        spa.shell.initModule( $container );
+        //$container.html(
+        //    '<h1 style="display:inline-block; margin:25px;">'
+        //    + 'hello world!'
+        //    + '</h1>'
+        //);
     };
     return { initModule: initModule };
 }());
